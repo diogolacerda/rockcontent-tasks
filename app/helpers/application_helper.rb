d9c1259dfca08a1185123c71ccff62f12394908d
Diogo Lacerda <diogo.lacerda@gmail.com>
2016-06-17 22:46:38 -0300

    Bootstrap configuration Application Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_message
+    messages = ""
+    [:info, :error].each { |type|
+      if flash[type]
+        messages += "<p class=\"#{type}\">#{flash[type]}</p>"
+      end
+    }
+
+    messages
+  end
 end
