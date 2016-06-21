20dea004b1ee210976fbb43d1df1812e92bb4d8c
Diogo Lacerda <diogo.lacerda@gmail.com>
2016-06-21 13:17:22 -0300

    Acceptance test for the task status change

--- spec/features/boards/board_index_spec.rb
+++ spec/features/boards/board_index_spec.rb
@@ -26,4 +26,39 @@ feature "Board Index" do
     expect(page).to have_content public_board.name
     expect(page).to have_content private_board.name
   end
+
+  scenario "should successfully change the task status", js: true do
+    to_do = create :task_status, name: "To-do"
+    in_progress = create :task_status, name: "In Progress"
+    public_board = create :board
+
+    to_do_task = create :task, name: "To-do Task", board: public_board, task_status: to_do
+    in_progress_task = create :task, name: "In Progress Task", board: public_board, task_status: in_progress
+
+    visit boards_path
+
+    within("ul#to_do_#{public_board.id}") do
+      expect(page).to have_content to_do_task.name
+      expect(page).not_to have_content in_progress_task.name
+    end
+    sleep 1
+
+    select "In Progress", from: "task_status_#{to_do_task.id}"
+
+    sleep 1
+
+    select "To-do", from: "task_status_#{in_progress_task.id}"
+
+    sleep 1
+    within("ul#to_do_#{public_board.id}") do
+      expect(page).not_to have_content to_do_task.name
+      expect(page).to have_content in_progress_task.name
+    end
+
+    within("ul#in_progress_#{public_board.id}") do
+      expect(page).to have_content to_do_task.name
+      expect(page).not_to have_content in_progress_task.name
+    end
+
+  end
 end
